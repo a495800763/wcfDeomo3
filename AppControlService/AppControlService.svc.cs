aa6eb5af2d73e3f4f5daf34cd44f60b3fcecf5a5
liumq <495800763@qq.com>
2021-11-27 22:20:51 +0800

    Add project files.

---
+++ AppControlService/AppControlService.svc.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using System.Windows;
+
+namespace AppControlService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "AppControlService" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select AppControlService.svc or AppControlService.svc.cs at the Solution Explorer and start debugging.
+    [ServiceBehavior(InstanceContextMode=InstanceContextMode.Single)]
+    public class AppControlService : IAppControlService
+    {
+        private MainWindow hostApp;
+
+        public AppControlService(MainW)
+
+        public void DoWork()
+        {
+        }
+    }
+}
---
+++ Client/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WCFDemo3;
+using System.ServiceModel;
+using static System.Console;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Press enter to begin.");
+            ReadLine();
+            IAppControlService client = ChannelFactory<IAppControlService>.CreateChannel(
+                new NetTcpBinding(),
+                new EndpointAddress("net.tcp://localhost:8081/AppControlService"));
+            WriteLine("Creating sun.");
+            client.SetRadius(100, "yellow", 3);
+            WriteLine("Press enter to begin.");
+            ReadLine();
+            WriteLine("Growing sun to red giant");
+            client.SetRadius(200, "red", 5);
+            WriteLine("Press enter to begin.");
+            ReadLine();
+            WriteLine("Collapsing sun to neutron star.");
+            client.SetRadius(50, "AliceBlue", 2);
+            WriteLine("Finished. Press enter to exit.");
+            ReadLine();
+        }
+    }
+}
